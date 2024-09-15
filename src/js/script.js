9d0a0cb7afcf9157ad2eb32e248b43889a4ebaf5
Max Klot <maxbklot@gmail.com>
2024-09-15 15:03:52 -0400

    Home Page

--- src/js/script.js
+++ src/js/script.js
@@ -7,15 +7,39 @@ function dropdown(){
     if(dropped == false){
         dropdown.style.display = "flex"
         dropdown.style.visibility = "visible"
-        albumtext.style.paddingTop = "50px"
+        albumtext.style.paddingTop = "50px";
         dropped = true
+
     }
     else{
-
+        albumtext.style.paddingTop = "250px";      
         dropdown.style.display = "none"
         dropdown.style.visibility = "collapse"
-        albumtext.style.paddingTop = "250px"
         dropped = false
     }
+}
+
+function iconhover(type){
+
+    switch(type){
+
+        case facebook:
+            const facebook = getElementById("facebook")
+            facebook.src = "resources/facebook_gray.png"
+            break;
+        case instagram:
+            const instagram = getElementById("instagram")
+            break;
+        case youtube: 
+        const youtube = getElementById("youtube")
+            break;
+        case tiktok:
+            const tiktok = getElementById("tiktok")
+            break;
+
+
+
+    }
+
 
 }
